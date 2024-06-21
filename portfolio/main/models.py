8468d7e883a96976ab67d7daa872cd7d10b86b44
Tommy Moua <tmoua996@gmail.com>
2024-06-21 13:32:35 -0500

    Initial project commit

---
+++ portfolio/main/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="projects")
+    link = models.URLField(max_length=200, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to="project_images/")
+
+    def __str__(self):
+        return f"{self.project.title} Image"
---
+++ portfolio/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag
+
+# Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    tags = Tag.objects.all()
+    return render(request, "home.html", {"projects": projects, "tags": tags})
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    project = get_object_or_404(Project, pk=id)
+    return render(request, "project.html", {"project": project})
