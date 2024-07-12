41e84197a5e2a3a091b725c6f93de2b064e5a101
Tommy Moua <tmoua996@gmail.com>
2024-07-11 20:50:41 -0500

    Added exception handling to views.py for database handling. Reinserted starting projects and their images.

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project, Tag
+from django.db import OperationalError
 
 # Create your views here.
 def home(request):
-    projects = Project.objects.all()
-    tags = Tag.objects.all().order_by("name")
+    try:
+        projects = Project.objects.all()
+        tags = Tag.objects.all().order_by("name")
+    except OperationalError:
+        projects = []
+        tags = []
     return render(request, "home.html", {"projects": projects, "tags": tags})
 
 def about(request):
     return render(request, "about.html")
 
 def project(request, id):
-    project = get_object_or_404(Project, pk=id)
+    try:
+        project = get_object_or_404(Project, pk=id)
+    except OperationalError:
+        return render(request, "project.html", {"error": "Database error occurred."})
     return render(request, "project.html", {"project": project})
