ed6b3d7f8026a34a68e34ea7dfd58589cc1760b9
Tommy Moua <tmoua996@gmail.com>
2024-09-07 19:09:10 -0500

    Added django's static files storage cache.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
